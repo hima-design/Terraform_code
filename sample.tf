ce2f125047b670d027e7f1ff10850a3caa0a4e64
Bindu <hima.bindu.ext@siemens.com>
2021-05-26 12:39:59 +0530

    modified

--- sample.tf
+++ sample.tf
@@ -3,7 +3,6 @@ provider "aws"{
     secret_key = "sL9mQRYn8P6+9xmuU+f/hLPiPJZIS7LcT4+QiPYx"
     region     = "us-east-1"
 }
-
 resource "aws_instance" "sample"{
     ami           ="ami-0d5eff06f840b45e9"
     instance_type ="t2.micro"
